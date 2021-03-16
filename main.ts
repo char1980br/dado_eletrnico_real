9c2323ddb4d179e04e9ea0f0e7843975c0bbaba7
char1980br <54154336+char1980br@users.noreply.github.com>
2021-03-16 15:30:45 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+let dado = 0
+input.onGesture(Gesture.Shake, function () {
+    dado = randint(1, 6)
+    if (dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dado == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dado == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dado == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dado == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dado == 6) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
